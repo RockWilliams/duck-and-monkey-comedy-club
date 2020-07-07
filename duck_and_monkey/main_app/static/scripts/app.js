43d65d7fa3ec7b3008da5f70bdd9d31754490212
RockW <rock7699@gmail.com>
2020-07-07 14:49:36 -0700

    commit so i can merge

--- duck_and_monkey/main_app/static/scripts/app.js
+++ duck_and_monkey/main_app/static/scripts/app.js
@@ -1,4 +1,12 @@
-// $('#modal').modal({
-//     backdrop: 'static',
-//     keyboard: true
-// })
+// function toggleMenu() {
+//     let getMenu = document.querySelector(".mainMenu");
+//     getMenu.classList.toggle("hamburger");
+// }
+// let getHamburger = document.querySelector("#toggle-bar");
+
+// getHamburger.addEventListener("click", toggleMenu);
+
+const $menu = $('#toggle-menu').on("click", function(event) {
+    console.log("Clicked")
+    $('.main-menu').toggle();
+});
